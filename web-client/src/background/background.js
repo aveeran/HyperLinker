3b1a458106e2e8e53b6756b9ce16d0834693e052
Abbinash Ranjitkar <abbinashranjitkar@gmail.com>
2024-08-21 18:59:01 -0600

    Reworked data flow in background.js; TODO: update data flow in dependent popup.jsx pages

--- web-client/src/background/background.js
+++ web-client/src/background/background.js
@@ -1,69 +1,91 @@
-let gameData = {
-    playing: false,
-    startTime: 0,
-    elapsedTime: 0,
-    pagesVisited: [],
-    nodeHistory: [],
-    edgeHistory: [],
-    currentNode: 0,
-    clickCount: 0
-  };
-  
-  let timerInterval;
+let singleplayerGame = {
+  singleplayerCustomizations: {},
+  playing: false,
+  startTime: 0,
+  tracking: 0,
+  path: [],
+  nodeHistory: [],
+  edgeHistory: [],
+};
 
-  chrome.storage.local.clear();
+let visitedPage = null;
 
+let timerInterval;
 
+chrome.storage.local.clear();
 
-  chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.action === "start_singleplayer") {
-      const { start, end } = message.data;
-      chrome.tabs.create({ url: start.link }, (newTab) => {
-        gameData.playing = true;
-        gameData.startTime = Date.now();
-        gameData.endPage = end.link;
-        gameData.clickCount = 0;
-        gameData.pagesVisited = [start.link]; // Initial page
-        chrome.storage.local.set({ "gameData": gameData});
-  
-        // Start tracking time
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action === "start_singleplayer") {
+    const { start, end } = message.data;
+    chrome.tabs.create({ url: start.link }, (newTab) => {
+      singleplayerGame.singleplayerCustomizations = message.data;
+      singleplayerGame.playing = true;
+      singleplayerGame.startTime = Date.now();
+
+      singleplayerGame.path = [
+        singleplayerGame.singleplayerCustomizations.start.link,
+        ...(singleplayerGame.singleplayerCustomizations.mode?.path?.intermediate_links.map(
+          (article) => article.link
+        ) || []),
+        singleplayerGame.singleplayerCustomizations.end.link,
+      ];
+
+      singleplayerGame.nodeHistory = Array.from(
+        { length: singleplayerGame.path.length },
+        () => []
+      );
+      chrome.storage.local.set({ "singleplayer-game": singleplayerGame });
+
+      if (singleplayerGame.singleplayerCustomizations.track === "time") {
         timerInterval = setInterval(() => {
-          if (gameData.playing) {
-            gameData.elapsedTime = Math.floor((Date.now() - gameData.startTime) / 1000);
-            chrome.storage.local.set({ elapsedTime: gameData.elapsedTime });
-            console.log(gameData.elapsedTime);
+          if (singleplayerGame.playing) {
+            const elapsedTime = Math.floor(
+              (Date.now() - singleplayerGame.startTime) / 1000
+            );
+            // chrome.storage.local.get("singleplayer-game", (result) => {
+            //   result["singleplayer-game"].track = elapsedTime;
+            //   chrome.storage.local.set("singleplayer-game", result["singleplayer-game"])
+            // })
+            chrome.storage.local.set({ elapsedTime: elapsedTime });
           }
         }, 1000);
-  
-        chrome.tabs.sendMessage(newTab.id, { action: "start_singleplayer", data: message.data });
+      }
+
+      // TEST SCRIPT TO SEND MESSAGE
+      chrome.tabs.sendMessage(newTab.id, {
+        action: "start_singleplayer",
+        data: message.data,
       });
-    }
-  
-    if (message.action === "wikipedia_click") {
-      if (gameData.playing) {
-        chrome.storage.local.get(['clickCount', 'pageVisited'], (result) => {
-          let clicks = result.clickCount || 0;
-          clicks++;
-          gameData.clickCount = clicks;
-  
+    });
+  }
+
+  if (message.action === "wikipedia_click") { // HOW SHOULD WE ADDRESS WIKIPEDIA CLICK?
+    if (singleplayerGame.playing) { // THE CONCERN IS HERE***
+      chrome.storage.local.get(["clickCount", "pageVisited"], (result) => {
+        let clicks = result.clickCount || 0;
+        clicks++;
+        // gameData.clickCount = clicks;
+
         //   let pagesVisited = result.pageVisited || '';
         //   pagesVisited.push(message.page);
         //   gameData.pagesVisited = pagesVisited;
-  
-          chrome.storage.local.set({ clickCount: clicks, pageVisited: message.page });
+
+        chrome.storage.local.set({
+          clickCount: clicks,
+          pageVisited: message.page,
         });
-      }
-    }
-  
-    if (message.action === "quit_singleplayer") {
-      gameData.playing = false;
-      clearInterval(timerInterval); // Stop the timer
-      chrome.storage.local.set({
-        clickCount: gameData.clickCount,
-        pagesVisited: gameData.pagesVisited,
-        elapsedTime: gameData.elapsedTime,
-        playing: false
       });
     }
-  });
-  
+  }
+
+  if (message.action === "quit_singleplayer") {
+    singleplayerGame.playing = false;
+    clearInterval(timerInterval); // Stop the timer
+    // chrome.storage.local.set({
+    //   clickCount: gameData.clickCount,
+    //   pagesVisited: gameData.pagesVisited,
+    //   elapsedTime: gameData.elapsedTime,
+    //   playing: false,
+    // });
+  }
+});
