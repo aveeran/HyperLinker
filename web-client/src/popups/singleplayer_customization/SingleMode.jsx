b86f8bbd5d5bd36a19279c73eced2bf6a9a4f4c4
Abbinash Ranjitkar <abbinashranjitkar@gmail.com>
2024-11-01 00:13:34 -0600

    Updated UI for mode

--- web-client/src/popups/singleplayer_customization/SingleMode.jsx
+++ web-client/src/popups/singleplayer_customization/SingleMode.jsx
@@ -139,71 +139,39 @@ function SingleMode() {
   };
 
   return (
-    <div>
-      <h1 className="text-4xl text-center mb-3">HyperLinker</h1>
-      <h2 className="text-3xl text-center mb-3">
-        Singleplayer - Customization
-      </h2>
-      <hr />
-      <div className=" p-1.5 m-3">
-        <p className="text-center mb-3">
-          {" "}
-          <strong>Mode</strong>
+    <div className="pt-3 p-1">
+      <h1 className="text-4xl text-center font-custom mb-3">HyperLinker</h1>
+      <div className="border-gray-400 border-2 border-solid p-1.5 m-3 bg-slate-100">
+        <p className="font-medium text-xl text-center bg-sky-200 p-1 mb-1">
+          Singleplayer
         </p>
-        <select
-          value={mode}
-          onChange={handleModeChange}
-          className="block mx-auto p-2 border rounded mb-3"
-        >
-          <option value="" disabled>
-            Select a mode
-          </option>
-          <option value="normal">Normal</option>
-          <option value="count-down">Count-Down</option>
-          <option value="path">Path</option>
-          <option value="hitler">Hitler</option>
-          <option value="jesus">Jesus</option>
-          <option value="random">Random</option>
-        </select>
-        {mode === "normal" ? (
-          <div className="flex flex-col border-gray border-2 border-solid p-2 mb-2">
-            <div className="flex mb-2 items-center">
-              <p className="w-[15%]">Start</p>
-              <div className="ml-5">
-                <SearchableDropdown
-                  onDataChange={updateFirstArticle}
-                  temp={startArticle}
-                />
-              </div>
-            </div>
-            <div className="flex mb-2 items-center">
-              <p className="w-[15%]">End</p>
-              <div className="ml-5">
-                <SearchableDropdown
-                  onDataChange={updateEndArticle}
-                  temp={endArticle}
-                />
-              </div>
-            </div>
+        <p className="text-center font-medium text-base bg-slate-200">
+          Customizations
+        </p>
+        <hr className="border-t-1 border-black m-3" />
+        <p className="text-center font-medium text-base bg-slate-200 mt-2 mb-2">
+          Mode
+        </p>
+
+        <div className="">
+          <div className="bg-yellow-200 p-1 mb-2">
+            <select
+              value={mode}
+              onChange={handleModeChange}
+              className="block mx-auto p-2 border rounded"
+            >
+              <option value="" disabled>
+                Select a mode
+              </option>
+              <option value="normal">Normal</option>
+              <option value="count-down">Count-Down</option>
+              <option value="path">Path</option>
+              <option value="hitler">Hitler</option>
+              <option value="jesus">Jesus</option>
+              <option value="random">Random</option>
+            </select>
           </div>
-        ) : null}
-
-        {mode === "count-down" ? (
-          <div className="items-center justify-center m-2">
-            <div className="flex items-center justify-center mr-2 mb-2 w-auto">
-              <input
-                className="flex text-center border rounded p-2 mr-2 max-w-[50%]"
-                type="number"
-                value={timer}
-                onChange={handleTimer}
-                min="1"
-                step="1"
-                placeholder="1"
-                aria-label="Count-down timer"
-              />
-              <span>seconds</span>
-            </div>
-
+          {mode === "normal" ? (
             <div className="flex flex-col border-gray border-2 border-solid p-2 mb-2">
               <div className="flex mb-2 items-center">
                 <p className="w-[15%]">Start</p>
@@ -224,72 +192,34 @@ function SingleMode() {
                 </div>
               </div>
             </div>
-          </div>
-        ) : null}
+          ) : null}
 
-        {mode === "path" ? (
-          <div className="flex flex-col items-center justify-center">
-            <div className="flex items-center justify-center">
-              <span className="mr-2">Path Length</span>
-              <input
-                className="text-center border rounded p-2"
-                type="number"
-                value={pathLength}
-                onChange={handlePathLength}
-                min="2"
-                step="1"
-                max="8"
-                placeholder="2"
-                aria-label="Positive integer input"
-              />
-            </div>
-
-            <div className="flex items-center justify-center p-2">
-              <label htmlFor="directed" className="">
+          {mode === "count-down" ? (
+            <div className="items-center justify-center m-2">
+              <div className="flex items-center justify-center mr-2 mb-2 w-auto">
                 <input
-                  type="checkbox"
-                  id="directed"
-                  value="directed"
-                  checked={isPathDirected}
-                  onChange={handleOptionChange}
-                  className="mr-2"
+                  className="flex text-center border rounded p-2 mr-2 max-w-[50%]"
+                  type="number"
+                  value={timer}
+                  onChange={handleTimer}
+                  min="1"
+                  step="1"
+                  placeholder="1"
+                  aria-label="Count-down timer"
                 />
-                <span>Directed Path</span>
-              </label>
-            </div>
+                <span>seconds</span>
+              </div>
 
-            <div className="items-center justify-center mb-2 border-gray border-2 border-solid p-2">
-              <p className="text-center mb-2">Path Links</p>
-
-              <div className="flex flex-col">
+              <div className="flex flex-col border-gray border-2 border-solid p-2 mb-2">
                 <div className="flex mb-2 items-center">
-                  <p className="w-[15%]">Start </p>
+                  <p className="w-[15%]">Start</p>
                   <div className="ml-5">
                     <SearchableDropdown
                       onDataChange={updateFirstArticle}
                       temp={startArticle}
-                      className=""
                     />
                   </div>
                 </div>
-
-                {Array.from({ length: pathLength - 2 }, (_, index) => (
-                  <div key={index} className="flex mb-2 items-center">
-                    <p className="w-[15%]">{index + 2}</p>
-                    <div className="ml-5">
-                      <SearchableDropdown
-                        key={index}
-                        onDataChange={updatePathArticles}
-                        index={index}
-                        temp={
-                          pathArticles[index] && pathArticles[index].suggestion
-                            ? pathArticles[index].suggestion
-                            : pathArticles[index]
-                        }
-                      />
-                    </div>
-                  </div>
-                ))}
                 <div className="flex mb-2 items-center">
                   <p className="w-[15%]">End</p>
                   <div className="ml-5">
@@ -301,54 +231,149 @@ function SingleMode() {
                 </div>
               </div>
             </div>
-          </div>
-        ) : null}
+          ) : null}
 
-        {mode === "hitler" ? (
-          <div className="flex flex-col items-center justify-center">
-            <SearchableDropdown onDataChange={updateFirstArticle} />
-            <div className="relative">
-              <input
-                type="text"
-                value={"Hitler"}
-                readOnly
-                className="p-2 border rounded w-full"
-                placeholder="Search Wikipedia..."
-              />
+          {mode === "path" ? (
+            <div className="">
+              <div className="flex items-center justify-center bg-purple-200 p-1">
+                <span className="mr-2 text-blue-700 font-semibold">
+                  Path Length
+                </span>
+                <input
+                  className="text-center border rounded p-2"
+                  type="number"
+                  value={pathLength}
+                  onChange={handlePathLength}
+                  min="2"
+                  step="1"
+                  max="8"
+                  placeholder="2"
+                  aria-label="Positive integer input"
+                />
+              </div>
+
+              <div className="bg-green-200 flex mt-2 mb-2 justify-center">
+                <label htmlFor="directed" className="">
+                  <input
+                    type="checkbox"
+                    id="directed"
+                    value="directed"
+                    checked={isPathDirected}
+                    onChange={handleOptionChange}
+                    className="mr-2"
+                  />
+                  <span className="font-semibold">Directed Path</span>
+                </label>
+              </div>
+
+              <div className="items-center justify-center mb-2">
+                <p className="text-center mb-2 bg-purple-200 p-1 text-blue-700 font-semibold">
+                  Path Links
+                </p>
+
+                <ul className="flex flex-col list-disc ml-16">
+                  <li className="mb-2 items-center justify-center list-item">
+                    <div className="flex items-center">
+                      <p className="w-[15%] font-medium">Start</p>
+                      <div className="">
+                        <SearchableDropdown
+                          onDataChange={updateFirstArticle}
+                          temp={startArticle}
+                          className=""
+                        />
+                    </div>
+                    </div>
+                  </li>
+
+                  {Array.from({ length: pathLength - 2 }, (_, index) => (
+                    <li
+                      key={index}
+                      className="mb-2 items-center justify-center list-item"
+                    >
+                      <div className="flex items-center">
+                        <p className="w-[15%] font-medium">{index + 2}</p>
+                        <div className="ml-5">
+                          <SearchableDropdown
+                            key={index}
+                            onDataChange={updatePathArticles}
+                            index={index}
+                            temp={
+                              pathArticles[index] &&
+                              pathArticles[index].suggestion
+                                ? pathArticles[index].suggestion
+                                : pathArticles[index]
+                            }
+                          />
+                      </div>
+                      </div>
+                    </li>
+                  ))}
+
+                  <li className="mb-2 items-center justify-center list-item">
+                    <div className="flex items-center">
+                      <p className="w-[15%] font-medium">End</p>
+                      <div className="">
+                        <SearchableDropdown
+                          onDataChange={updateEndArticle}
+                          temp={endArticle}
+                        />
+                    
+                    </div>
+                    </div>
+                  </li>
+                </ul>
+
+                
+              </div>
             </div>
-          </div>
-        ) : null}
+          ) : null}
 
-        {mode === "jesus" ? (
-          <div className="flex flex-col items-center justify-center">
-            <SearchableDropdown onDataChange={updateFirstArticle} />
-            <div className="relative">
-              <input
-                type="text"
-                value={"Jesus"}
-                readOnly
-                className="p-2 border rounded w-full"
-                placeholder="Search Wikipedia..."
-              />
+          {mode === "hitler" ? (
+            <div className="flex flex-col items-center justify-center">
+              <SearchableDropdown onDataChange={updateFirstArticle} />
+              <div className="relative">
+                <input
+                  type="text"
+                  value={"Hitler"}
+                  readOnly
+                  className="p-2 border rounded w-full"
+                  placeholder="Search Wikipedia..."
+                />
+              </div>
             </div>
+          ) : null}
+
+          {mode === "jesus" ? (
+            <div className="flex flex-col items-center justify-center">
+              <SearchableDropdown onDataChange={updateFirstArticle} />
+              <div className="relative">
+                <input
+                  type="text"
+                  value={"Jesus"}
+                  readOnly
+                  className="p-2 border rounded w-full"
+                  placeholder="Search Wikipedia..."
+                />
+              </div>
+            </div>
+          ) : null}
+
+          {mode === "random" ? <div></div> : null}
+
+          <div className="flex justify-center mb-3">
+            <button
+              className="flex bg-gray-400 text-white px-4 py-2 rounded mr-3"
+              onClick={handleBack}
+            >
+              Return
+            </button>
+            <button
+              className="flex bg-blue-400 text-white px-4 py-2 rounded"
+              onClick={handleSubmit}
+            >
+              Save
+            </button>
           </div>
-        ) : null}
-
-        {mode === "random" ? <div></div> : null}
-
-        <div className="flex justify-center mb-3">
-          <button
-            className="flex bg-gray-400 text-white px-4 py-2 rounded mr-3"
-            onClick={handleBack}
-          >
-            Return
-          </button>
-          <button
-            className="flex bg-blue-400 text-white px-4 py-2 rounded"
-            onClick={handleSubmit}
-          >
-            Save
-          </button>
         </div>
       </div>
     </div>
